2433de57dce00a32ffea87d4daf60f08f6aec113
Kenner <kenner@pop-os.localdomain>
2020-05-01 18:10:51 +0100

    Update of migration

--- BatchProcessor.Data/Migrations/20200501161409_Change in database.Designer.cs
+++ BatchProcessor.Data/Migrations/20200501161409_Change in database.Designer.cs
@@ -4,14 +4,16 @@ using BatchProcessor.Data.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BatchProcessor.Data.Migrations
 {
     [DbContext(typeof(BPContext))]
-    partial class BPContextModelSnapshot : ModelSnapshot
+    [Migration("20200501161409_Change in database")]
+    partial class Changeindatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
