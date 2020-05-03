cb69959388e87304a400d96c124eaa20555ecd62
Kenner <kenner@pop-os.localdomain>
2020-05-03 13:17:06 +0100

    Implemented method to refresh table every two secounds

--- BatchProcessorAPP/src/app/input/input-grid/input-grid.component.ts
+++ BatchProcessorAPP/src/app/input/input-grid/input-grid.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { InputService } from './../shared/input.service';
 import { BatchLot } from './../shared/batch-lot.model';
-import { Batch } from './../shared/batch.model';
+import { Subscription, timer } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-input-grid',
@@ -11,12 +12,19 @@ import { Batch } from './../shared/batch.model';
 export class InputGridComponent implements OnInit {
 
   public batchLot: BatchLot;
+  subscription: Subscription;
 
   constructor(public service: InputService) { }
 
   ngOnInit(): void {
-    this.service.refreshGrid().subscribe(data => this.batchLot = data);
-    console.log(this.batchLot);
+    this.subscription = timer(0, 2000).pipe(
+      switchMap(() => this.service.refreshGrid())
+    ).subscribe(data => this.batchLot = data);
+    //this.service.refreshGrid().subscribe(data => this.batchLot = data);
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
   }
 
 }
