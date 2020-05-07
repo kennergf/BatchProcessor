a212268a110db36894fd39b09ced56f66895b89c
Kenner <kenner@pop-os.localdomain>
2020-05-07 15:19:33 +0100

    Removed unecessary things

--- BatchProcessor.API/Startup.cs
+++ BatchProcessor.API/Startup.cs
@@ -1,21 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using BatchProcessor.API.Services;
 using BatchProcessor.Data.Data;
-using BatchProcessor.Data.Entities;
 using BatchProcessor.Data.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace BatchProcessor.API
 {
